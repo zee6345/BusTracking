419527a954981f7f139d543453f29ba910e371ff
AyyazTalat <ayaztalat@builtinsoft.com>
2023-09-03 22:46:43 +0500

    add country code

--- app/src/main/java/com/app/bustracking/presentation/views/fragments/main/LoginFragment.kt
+++ app/src/main/java/com/app/bustracking/presentation/views/fragments/main/LoginFragment.kt
@@ -1,9 +1,9 @@
 package com.app.bustracking.presentation.views.fragments.main
 
+import android.annotation.SuppressLint
 import android.app.ProgressDialog
 import android.os.Bundle
 import android.text.TextUtils
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,11 +19,10 @@ import com.app.bustracking.presentation.views.fragments.BaseFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 
 class LoginFragment : BaseFragment() {
 
-    private lateinit var binding:FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
     private lateinit var navController: NavController
 
     override fun initNavigation(navController: NavController) {
@@ -38,45 +37,56 @@ class LoginFragment : BaseFragment() {
         return binding.root
     }
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonLogin.setOnClickListener {
             //navController.navigate(R.id.action_loginFragment_to_selectNetwrokFragment)
-        val progressDialog = ProgressDialog(requireContext())
-            progressDialog.setTitle("Loading")
+            val progressDialog = ProgressDialog(requireContext())
+//            progressDialog.setTitle("Loading")
+            progressDialog.setMessage("Loading...")
 
-            val number  = binding.phoneNumber.text.toString().trim()
-            if (TextUtils.isEmpty(number)){
-                binding.phoneNumber.error =" Please enter number"
-            }else {
+            val number = binding.phoneNumber.text.toString().trim()
+            val code = binding.ccp.selectedCountryCode.toString().trim()
+
+            if (TextUtils.isEmpty(number)) {
+                binding.phoneNumber.error = " Please enter number"
+            } else {
                 progressDialog.show()
                 val apiService = ApiClient.createService().create(ApiService::class.java)
-                apiService.login(number).enqueue(object :Callback<LoginModel>{
+                apiService.login("+$code$number").enqueue(object : Callback<LoginModel> {
                     override fun onResponse(
                         call: Call<LoginModel>,
                         response: Response<LoginModel>
                     ) {
                         progressDialog.dismiss()
-                        if (response.isSuccessful && response.code() == 200){
+                        if (response.isSuccessful && response.code() == 200) {
                             val model = response.body() as LoginModel
                             AppPreference.setToken(model.token)
 
                             val bundle = Bundle()
-                            bundle.putString("number", number)
+                            bundle.putString("number", "+$code$number")
 
-                            navController.navigate(R.id.action_loginFragment_to_verifyOTP,bundle)
+                            navController.navigate(R.id.action_loginFragment_to_verifyOTP, bundle)
 
-                        }else{
-//                            val model = response.body() as LoginModel
-                            Toast.makeText(requireContext(),"Failed",Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                "Failed to sign in!",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
 
                     override fun onFailure(call: Call<LoginModel>, t: Throwable) {
                         progressDialog.dismiss()
-                       t.printStackTrace()
-                        Toast.makeText(requireContext(),"Failed",Toast.LENGTH_SHORT).show()
+                        t.printStackTrace()
+                        Toast.makeText(
+                            requireContext(),
+                            "Something went wrong!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
 
                 })
